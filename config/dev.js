70e53e3dbdebaa00763b3648e537fcf45cd5960b
Brijendra <getBrijendra@gmail.com>
2021-02-21 11:08:48 +0530

    adding dummy devkeys

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+      '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb+srv://user1:user1@cluster0.dawwz.mongodb.net/blog_dev?retryWrites=true&w=majority', 
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIA4UL75UCB6A74IHT',
+  secretAccessKey: 'JlBbaI3QoaDrT43kb2WBPQvh6+6d+d0IasBnw9oi'
+};
